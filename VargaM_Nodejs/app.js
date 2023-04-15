dc6000cefcc00e4a32d106a2f4181b0c2febbabb
matias10008 <vargamate005@gmail.com>
2023-04-15 18:54:01 +0200

    Mongoose added

--- VargaM_Nodejs/app.js
+++ VargaM_Nodejs/app.js
@@ -1,11 +1,20 @@
-const express = require("express")
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose')
+const app = express();
+const port = process.env.port || 3000;
+const dbUrl = 'mongodb+srv://admin:asdasdasd@prf-beadando-cluster.m6x1cnh.mongodb.net/test'
+
+mongoose.connect(dbUrl);
+
+
+app.use(cors());
 
 app.use((req, res, next) => {
 
-    console.log('A middleware futott!')
+    console.log('A middleware futott!');
 
-    next()
+    next();
   })
 
 
@@ -15,5 +24,5 @@ app.use('', express.static('public'))
 
 
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
+  console.log('Server is running on http://localhost:3000');
 })
