e80051d8d11f923c225cbd453d926c5ce36173bb
matias10008 <vargamate005@gmail.com>
2023-04-21 18:47:40 +0200

    NodeJS update

--- VargaM_Nodejs/app.js
+++ VargaM_Nodejs/app.js
@@ -2,13 +2,25 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose')
 const app = express();
+const passport = require('passport')
+const expressSession = require('express-session')
+const localStrategy = require('passport-local').Strategy;
+
+
 const port = process.env.port || 3000;
 const dbUrl = 'mongodb+srv://admin:asdasdasd@prf-beadando-cluster.m6x1cnh.mongodb.net/test'
+
 
 mongoose.connect(dbUrl);
 
 
+
 app.use(cors());
+
+app.use(expressSession({secret:'idkwhattodo', resave:true}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use((req, res, next) => {
 
